9bd0293948670e1e18de8ea9261e2d54d114d3b2
Neyko Naydenov <naydenovneyko@gmail.com>
2024-12-04 20:47:10 +0200

    updated version to 0.14

--- src/math/vec3.zig
+++ src/math/vec3.zig
@@ -4,35 +4,35 @@ pub const Vec3 = packed struct {
     x: f32,
     y: f32,
     z: f32,
-    pub fn init(x: f32, y: f32, z: f32) Vec3 {
+    pub fn Init(x: f32, y: f32, z: f32) Vec3 {
         return Vec3{ .x = x, .y = y, .z = z };
     }
+    pub inline fn Length(self: Vec3) f32 {
+        return std.math.sqrt(self.x * self.x + self.y * self.y + self.z * self.z);
+    }
+    pub inline fn SubScalerFromVec(val: f32, v: Vec3) Vec3 {
+        return Vec3.Init(val - v.x, val - v.y, val - v.z);
+    }
+    pub inline fn SubVecFromVec(self: Vec3, v: Vec3) Vec3 {
+        return Vec3.Init(self.x - v.x, self.y - v.y, self.z - v.z);
+    }
+    pub inline fn ScaleVecByVec(self: Vec3, v: Vec3) Vec3 {
+        return Vec3.Init(self.x * v.x, self.y * v.y, self.z * v.z);
+    }
+    pub inline fn ScaleVecByT(t: f32, vec: Vec3) Vec3 {
+        return Vec3.Init(t * vec.x, t * vec.y, t * vec.z);
+    }
+    pub inline fn AddVecToVec(self: Vec3, v: Vec3) Vec3 {
+        return Vec3.Init(self.x + v.x, self.y + v.y, self.z + v.z);
+    }
+    pub inline fn DotProduct(self: Vec3, v: Vec3) f32 {
+        return (self.x * v.x + self.y * v.y + self.z * v.z);
+    }
+    pub inline fn CrossProduct(self: Vec3, v: Vec3) Vec3 {
+        return Vec3.Init((self.y * v.z) - (self.z * v.y), (self.z * v.x) - (self.x * v.z), (self.x * v.y) - (self.y * v.x));
+    }
+    pub inline fn UnitVector(u: Vec3) Vec3 {
+        const len = Length(u);
+        return Vec3.Init((u.x / len), (u.y / len), (u.z / len));
+    }
 };
-pub inline fn SubScalerFromVec(val: f32, v: Vec3) Vec3 {
-    return Vec3.init(val - v.x, val - v.y, val - v.z);
-}
-pub inline fn SubVecFromVec(u: Vec3, v: Vec3) Vec3 {
-    return Vec3.init(u.x - v.x, u.y - v.y, u.z - v.z);
-}
-pub inline fn length(vec: Vec3) f32 {
-    return std.math.sqrt(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
-}
-pub inline fn ScaleVecByVec(u: Vec3, v: Vec3) Vec3 {
-    return Vec3.init(u.x * v.x, u.y * v.y, u.z * v.z);
-}
-pub inline fn ScaleVecByT(t: f32, vec: Vec3) Vec3 {
-    return Vec3.init(t * vec.x, t * vec.y, t * vec.z);
-}
-pub inline fn AddVecToVec(u: Vec3, v: Vec3) Vec3 {
-    return Vec3.init(u.x + v.x, u.y + v.y, u.z + v.z);
-}
-pub inline fn DotProduct(u: Vec3, v: Vec3) f32 {
-    return (u.x * u.x + u.y * v.y + u.z * v.z);
-}
-pub inline fn CrossProduct(u: Vec3, v: Vec3) Vec3 {
-    return Vec3.init((u.y * v.z) - (u.z * v.y), (u.z * v.x) - (u.x * v.z), (u.x * v.y) - (u.y * v.x));
-}
-pub inline fn UnitVector(u: Vec3) Vec3 {
-    const len = length(u);
-    return Vec3.init((u.x / len), (u.y / len), (u.z / len));
-}
